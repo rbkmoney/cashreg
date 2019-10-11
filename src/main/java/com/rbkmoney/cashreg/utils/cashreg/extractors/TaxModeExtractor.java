cc481035572275f93d2598b97c14f3858d5fae16
avcherkasov <shnurok14@ya.ru>
2019-10-11 11:32:42 +0300

    PROX-313: fix tax mode

--- src/main/java/com/rbkmoney/cashreg/utils/cashreg/extractors/TaxModeExtractor.java
+++ src/main/java/com/rbkmoney/cashreg/utils/cashreg/extractors/TaxModeExtractor.java
@@ -1,5 +1,6 @@
 package com.rbkmoney.cashreg.utils.cashreg.extractors;
 
+import com.rbkmoney.cashreg.service.management.model.ExtraField;
 import com.rbkmoney.damsel.cashreg_domain.TaxMode;
 
 import java.util.Map;
@@ -9,10 +10,10 @@ import static com.rbkmoney.cashreg.service.management.model.ExtraField.TAX_MODE;
 public class TaxModeExtractor {
 
     public static TaxMode extractTaxModeFromOptions(Map<String, String> options) {
-        if (!options.containsKey(TAX_MODE)) {
+        if (!options.containsKey(ExtraField.TAX_MODE.getField())) {
             throw new RuntimeException("RussianLegalEntity does not contain " + TAX_MODE);
         }
-        return TaxMode.valueOf(options.get(TAX_MODE));
+        return TaxMode.valueOf(options.get(ExtraField.TAX_MODE.getField()));
     }
 
 }
